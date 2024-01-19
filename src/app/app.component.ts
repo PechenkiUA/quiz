85f29e22a87330d49832a2794205de98cc6a9998
Sasha <alexandrkrot1993@gmail.com>
2024-01-19 21:42:38 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {Quiz} from "ngx-quiz";
+// @ts-ignore
+import questions from './questions.json';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IOC-SurveyJS';
+
+  quiz: Quiz = {
+    name: 'Питання генерував Chat GPT я не винен',
+    level: 'У вас ',
+    duration: 480,
+    automaticAdvance: false,
+    returnAllowed: true,
+    repeatAllowed: true,
+    emitReport: true,
+    startScreen: {
+      title: '',
+      description: '',
+      displayImage:
+        'https://cdn.analyticsvidhya.com/wp-content/uploads/2023/04/ai-generated-gba2dce9e3_1920_xMPNobD.jpg',
+      buttonText: 'Поїхати'
+    },
+    endScreen: {
+      title: 'Ой молодець',
+      description: 'Завершено',
+      displayImage:
+        'https://cdn.analyticsvidhya.com/wp-content/uploads/2023/04/ai-generated-gba2dce9e3_1920_xMPNobD.jpg',
+      buttonText: 'Знову'
+    },
+    questions:  questions
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxQuizModule } from 'ngx-quiz';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxQuizModule
   ],
   providers: [],
   bootstrap: [AppComponent]
